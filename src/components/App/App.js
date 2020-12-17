0f3c55621e42bd02946ec68e8ac73e205b88a046
Tatiana Eskina <horechek444@gmail.com>
2020-12-17 20:06:52 +0300

    feat: начало работы, подключение шрифтов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

a5de15c9dee89338dfdd1bfbf7d0e56796d33dfa
Tatiana Eskina <horechek444@gmail.com>
2020-12-29 14:43:37 +0300

    feat: продолжение верстки tablet

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,14 +4,16 @@ import './SearchForm.css';
 const SearchForm = () => {
   return (
     <section className="search-form">
-      <h1 className="search-form__title">Что творится в мире?</h1>
-      <p className="search-form__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном
-        кабинете.</p>
-      <div className="search-form__cover">
-        <label className="search-form__control">
-          <input className="search-form__input" type="text" placeholder="Введите тему новости"/>
-        </label>
-        <input className="search-form__submit" type="submit" value="Искать"/>
+      <div className="search-form__container">
+        <h1 className="search-form__title">Что творится в мире?</h1>
+        <p className="search-form__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном
+          кабинете.</p>
+        <div className="search-form__cover">
+          <label className="search-form__control">
+            <input className="search-form__input" type="text" placeholder="Введите тему новости"/>
+          </label>
+          <input className="search-form__submit" type="submit" value="Искать"/>
+        </div>
       </div>
     </section>
   )
